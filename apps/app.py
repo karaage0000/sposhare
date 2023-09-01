c174908f27bea2a08afa94fcea000ecd2343e95a
karaage0000 <karaagekun0451@gmail.com>
2023-09-01 13:35:54 +0900

    9/1更新(4回目)

--- apps/app.py
+++ apps/app.py
@@ -7,14 +7,20 @@ from flask import Flask
 # Flaskのインスタンスを生成
 app = Flask(__name__)
 # 設定ファイルを読み込む
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['CLEARDB_DATABASE_URL']
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['CLEARDB_DATABASE_URL']
 #app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:pass@localhost/our_users'
-app.config['SECRET_KEY'] = os.urandom(10)
+
 
 """SQLAlchemyの登録
 """
 # SQLAlchemyのインスタンスを生成
 from flask_sqlalchemy import SQLAlchemy
+# Herokuの環境変数からCLEARDB_DATABASE_URLを取得
+db_url = os.environ.get('CLEARDB_DATABASE_URL')
+
+# Flaskアプリケーションにデータベース接続情報を設定
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SECRET_KEY'] = os.urandom(10)
 db = SQLAlchemy(app)# SQLAlchemyオブジェクトにFlaskオブジェクトを登録する
 #db.init_app(app)
 
